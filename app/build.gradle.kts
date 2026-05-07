a9338575f9d0e9e3814de879162bea68cdd20090
unknown <your.email@example.com>
2026-05-07 11:01:14 +0700

    Lower minSdk to 21 to support Android 5.0+

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yourdomain.displayboard"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
